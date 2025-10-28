06c2e0abc97000ee2659ae08a2dfdbad3fd5fb3b
Juan Pablo <juanpablo10056@gmail.com>
2025-10-28 10:35:28 -0500

    Se implementacion las busquedas por filtro y por dato especifico en UserController

--- src/main/java/co/edu/uco/nose/controller/UserController.java
+++ src/main/java/co/edu/uco/nose/controller/UserController.java
@@ -3,6 +3,8 @@ package co.edu.uco.nose.controller;
 import co.edu.uco.nose.business.facade.impl.UserFacadeImpl;
 import co.edu.uco.nose.controller.dto.Response;
 import co.edu.uco.nose.crosscuting.exception.NoseException;
+import co.edu.uco.nose.dto.CityDTO;
+import co.edu.uco.nose.dto.IdTypeDTO;
 import co.edu.uco.nose.dto.UserDTO;
 
 import org.springframework.http.HttpStatus;
@@ -15,8 +17,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -24,7 +28,7 @@ import java.util.UUID;
 public class UserController {
 
     @GetMapping
-    public ResponseEntity<Response<UserDTO>> findAllUsers(){
+    public ResponseEntity<Response<UserDTO>> findAllUsers() {
 
         Response<UserDTO> responseObjectData = Response.createSuccededResponse();
         HttpStatusCode responseStatusCode = HttpStatus.OK;
@@ -51,6 +55,89 @@ public class UserController {
         }
         return new ResponseEntity<Response<UserDTO>>(responseObjectData, responseStatusCode);
     }
+
+        @GetMapping("/{id}")
+        public ResponseEntity<Response<UserDTO>> findSpecificUser(@PathVariable("id") UUID id) {
+
+            Response<UserDTO> responseObjectData = Response.createSuccededResponse();
+            HttpStatusCode responseStatusCode = HttpStatus.OK;
+
+            try {
+                var facade = new UserFacadeImpl();
+
+                responseObjectData.setData(List.of(facade.findSpecificUser(id)));
+                responseObjectData.addMessage("Find Specific User successfully");
+
+            } catch (final NoseException exception) {
+                responseObjectData = Response.createFailedResponse();
+                responseObjectData.addMessage(exception.getUserMessage());
+                responseStatusCode = HttpStatus.NOT_FOUND;
+                exception.printStackTrace();
+
+            } catch (final Exception exception) {
+                var userMessage = "Unexpected error";
+                responseObjectData = Response.createFailedResponse();
+                responseObjectData.addMessage(userMessage);
+                responseStatusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+                exception.printStackTrace();
+            }
+            return new ResponseEntity<>(responseObjectData, responseStatusCode);
+        }
+
+        @GetMapping("/filter")
+        public ResponseEntity<Response<UserDTO>> findUsersByFilter(
+                @RequestParam(required = false) UUID id,
+                @RequestParam(required = false) String idNumber,
+                @RequestParam(required = false) String email,
+                @RequestParam(required = false) String firstName,
+                @RequestParam(required = false) String mobileNumber,
+                @RequestParam(required = false) UUID idTypeId,
+                @RequestParam(required = false) UUID homeCityId
+        ) {
+            Response<UserDTO> responseObjectData = Response.createSuccededResponse();
+            HttpStatusCode responseStatusCode = HttpStatus.OK;
+
+            try {
+                var facade = new UserFacadeImpl();
+
+                UserDTO filter = new UserDTO();
+                filter.setId(id);
+                filter.setIdNumber(idNumber);
+                filter.setEmail(email);
+                filter.setFirstName(firstName);
+                filter.setMobileNumber(mobileNumber);
+
+                if (idTypeId != null) {
+                    IdTypeDTO idTypeFilter = new IdTypeDTO();
+                    idTypeFilter.setId(idTypeId);
+                    filter.setIdType(idTypeFilter);
+                }
+
+                if (homeCityId != null) {
+                    CityDTO cityFilter = new CityDTO();
+                    cityFilter.setId(homeCityId);
+                    filter.setHomeCity(cityFilter);
+                }
+
+                responseObjectData.setData(facade.findUsersByFilter(filter));
+                responseObjectData.addMessage("Users filtered succesfully");
+
+            } catch (final NoseException exception) {
+                responseObjectData = Response.createFailedResponse();
+                responseObjectData.addMessage(exception.getUserMessage());
+                responseStatusCode = HttpStatus.BAD_REQUEST;
+                exception.printStackTrace();
+
+            } catch (final Exception exception) {
+                var userMessage = "Unexpected error";
+                responseObjectData = Response.createFailedResponse();
+                responseObjectData.addMessage(userMessage);
+                responseStatusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+                exception.printStackTrace();
+            }
+            return new ResponseEntity<>(responseObjectData, responseStatusCode);
+        }
+
 
     @PostMapping
     public ResponseEntity<Response<UserDTO>> registerNewUserInformation(@RequestBody UserDTO user) {
